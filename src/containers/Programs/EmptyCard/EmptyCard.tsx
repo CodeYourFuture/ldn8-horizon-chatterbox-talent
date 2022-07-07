95e033d0d789e8cf855626db3befa855d54b8ca1
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-07-07 11:10:41 +0100

    ui complete

--- src/containers/Programs/EmptyCard/EmptyCard.tsx
+++ src/containers/Programs/EmptyCard/EmptyCard.tsx
@@ -7,15 +7,24 @@ import styles from './EmptyCard.module.scss';
 
 const EmptyCard = () => {
     const [showPopup, setShowPopup] = useState(false);
+    const [programSearchQuery, setProgramSearchQuery] = useState("")
 
     const handleShowPopup = (option: boolean) => {
         setShowPopup(option);
+    }
+
+    const handleSearch = () => {
+        setProgramSearchQuery("")
     }
 
     return (
         <div className={styles.emptyCardWrapper}>
             <h1 className={styles.headlines}>Want to be the first to hear about refugee schemes and opportunities?</h1>
             <SlideButton buttonText='Subscribe' onClick={() => handleShowPopup(true)}/>
+            <div className={styles.search}>
+                <input type="text" name="search" id="search" value={programSearchQuery} onChange={(e) => setProgramSearchQuery(e.target.value)} />
+                <button className={styles.searchButton} onClick={handleSearch}>Search</button>
+            </div>
             {showPopup && <MailChimPopUp onSuccess={() => handleShowPopup(false)} onClose={() => handleShowPopup(false)}/>}
         </div>
     )
