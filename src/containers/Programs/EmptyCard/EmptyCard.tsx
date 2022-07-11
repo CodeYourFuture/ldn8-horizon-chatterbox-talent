78e1c36c8433592d182d2a7d67216cc75d54f2d1
daniel piga <daniel.piga@sainsburys.co.uk>
2022-07-11 21:23:23 +0100

    Fix errors

--- src/containers/Programs/EmptyCard/EmptyCard.tsx
+++ src/containers/Programs/EmptyCard/EmptyCard.tsx
@@ -1,7 +1,3 @@
-import React, { useState } from 'react';
-
-//import MailChimPopUp from '../../../components/MailChimpPopUp';
-
 import SlideButton from '../../../components/SlideButton/SlideButton';
 
 import styles from './EmptyCard.module.scss';
@@ -13,18 +9,11 @@ interface EmptyCardProps {
 
 
 const EmptyCard = ({handleShowPopup}:EmptyCardProps) => {
-    const dispatch = useDispatch()
-    const [programSearchQuery, setProgramSearchQuery] = useState("")
-
+    
     return (
         <div className={styles.emptyCardWrapper}>
             <h1 className={styles.headlines}>Want to be the first to hear about refugee schemes and opportunities?</h1>
             <SlideButton buttonText='Subscribe' onClick={() => handleShowPopup(true)}/>
-            <div className={styles.search}>
-                <input type="text" name="search" id="search" value={programSearchQuery} onChange={(e) => setProgramSearchQuery(e.target.value)} />
-                <button className={styles.searchButton} onClick={handleSearch}>Search</button>
-            </div>        
-            
         </div>
     )
 }
