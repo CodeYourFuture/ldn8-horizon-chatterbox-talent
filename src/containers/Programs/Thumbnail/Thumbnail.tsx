027d0a0426d057e58a3b6c2509eb8c5be58c7579
Margarita Glushkova <m.glushkova1@gmail.com>
2022-07-20 15:40:40 +0100

    Update Thumbnail.tsx

--- src/containers/Programs/Thumbnail/Thumbnail.tsx
+++ src/containers/Programs/Thumbnail/Thumbnail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,} from 'react';
 import { connect, useSelector } from 'react-redux';
 import { ClipLoader } from 'react-spinners';
 import styled from 'styled-components';
